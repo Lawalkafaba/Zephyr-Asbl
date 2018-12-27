7d9dac8989d8f0c0965fbd6d68d170a4984c52e0
krauseangelique <krause_angelique@hotmail.com>
2018-12-27 12:54:49 +0100

    mediaqueries slider

--- js/main.js
+++ js/main.js
@@ -1,13 +1,26 @@
 
 jQuery( document ).ready(function($) {
  
- 
-    $('.owl-carousel').owlCarousel({
+ if (window.innerWidth > 560) {
+   $('.owl-carousel').owlCarousel({
        loop:true,
        margin:10,
        nav:false,
-       
+        responsive:{
+            0:{
+                items:1,           
+            }, 
+            560:{
+                items:1,           
+            }, 
+            1000:{
+                items:5,                
+            }
+        }
    });
+}
+ 
+  
  
 });
 
